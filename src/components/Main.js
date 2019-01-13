34a597bd189a631684bd5a550a4ecee844aef682
AS_P <aparfenov154@gmail.com>
2019-01-13 19:41:37 +0700

    третий день

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,8 @@ import Col from 'react-bootstrap/lib/Col';
 
 import styled from 'styled-components';
 
+import Slider from './Slider';
+
 const Repair = styled.div`
 	color: #ffffff;
 	font-weight: 700;
@@ -48,7 +50,7 @@ class Main extends React.Component {
 					<CallBtn>Отправить заявку!</CallBtn>
 				</Col>
 				<Col lg={6} lgOffset={1}>
-
+					<Slider />
 				</Col>
 			</Row>
 		)
